d8ecaad6f97fdd772bdecd4cddf1af80e4becc87
ngoclinh2812 <hngoclinh2812@gmail.com>
2023-06-12 14:37:56 +0700

    Linh's updates.

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -60,128 +60,121 @@ const Register = () => {
         setConfirmPasswordVisible(!confirmPasswordVisible);
     };
 
-    // redirect authenticated user to the login page
-    useEffect(() => {
-        if (userInfo) {
-            navigate("/login"); // Update the path to "/"
-        }
-    }, [userInfo]);
-
     return (
         <>
-        <div className=" mx-auto bg-white p-8 my-4 rounded shadow-md">
-            {loading && <SpinnerLoading />}
-            <div className="flex justify-center">
-                <div className="w-1/2">
-                    <h2 className="text-2xl mb-4">Register</h2>
-                    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                        <Form className="space-y-4">
-                            <div className="flex space-x-4">
-                                <div className="w-1/2">
-                                    <label htmlFor="firstName">First Name</label>
-                                    <Field
-                                        type="text"
-                                        id="firstName"
-                                        name="firstName"
-                                        className="w-full border border-gray-300 rounded px-3 py-2"
-                                    />
-                                    <ErrorMessage name="firstName" component="span" className="text-red-500" />
-                                </div>
-                                <div className="w-1/2">
-                                    <label htmlFor="lastName">Last Name</label>
-                                    <Field
-                                        type="text"
-                                        id="lastName"
-                                        name="lastName"
-                                        className="w-full border border-gray-300 rounded px-3 py-2"
-                                    />
-                                    <ErrorMessage name="lastName" component="span" className="text-red-500" />
-                                </div>
-                            </div>
-                            <div>
-                                <label htmlFor="phone">Phone</label>
-                                <Field type="text" id="phone" name="phone" className="w-full border border-gray-300 rounded px-3 py-2" />
-                                <ErrorMessage name="phone" component="span" className="text-red-500" />
-                            </div>
-                            <div>
-                                <label htmlFor="email">Email</label>
-                                <Field
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    className="w-full border border-gray-300 rounded px-3 py-2"
-                                />
-                                <ErrorMessage name="email" component="span" className="text-red-500" />
-                            </div>
-                            <div>
-                                <label htmlFor="username">Username</label>
-                                <Field
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    className="w-full border border-gray-300 rounded px-3 py-2"
-                                />
-                                <ErrorMessage name="username" component="span" className="text-red-500" />
-                            </div>
-
-                            <div className="flex space-x-4">
-                                <div className="w-1/2">
-                                    <label htmlFor="password">Password</label>
-                                    <div className="relative">
+            <div className=" mx-auto bg-white p-8 my-4 rounded shadow-md">
+                {loading && <SpinnerLoading />}
+                <div className="flex justify-center">
+                    <div className="w-1/2">
+                        <h2 className="text-2xl mb-4">Register</h2>
+                        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                            <Form className="space-y-4">
+                                <div className="flex space-x-4">
+                                    <div className="w-1/2">
+                                        <label htmlFor="firstName">First Name</label>
                                         <Field
-                                            type={passwordVisible ? 'text' : 'password'}
-                                            id="password"
-                                            name="password"
+                                            type="text"
+                                            id="firstName"
+                                            name="firstName"
                                             className="w-full border border-gray-300 rounded px-3 py-2"
                                         />
-                                        <button
-                                            type="button"
-                                            className="absolute top-1/2 right-2 transform -translate-y-1/2"
-                                            onClick={togglePasswordVisibility}
-                                        >
-                                            {passwordVisible ?
-                                                <>
-                                                    <svg className="w-6 h-6" fill="none" stroke="currentColor"
-                                                         viewBox="0 0 24 24"
-                                                         xmlns="http://www.w3.org/2000/svg">
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                              strokeWidth="2"
-                                                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                              strokeWidth="2"
-                                                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-                                                        </path>
-                                                    </svg>
-                                                </>
-                                                :
-                                                <>
-                                                    <svg className="w-6 h-6" fill="none" stroke="currentColor"
-                                                         viewBox="0 0 24 24"
-                                                         xmlns="http://www.w3.org/2000/svg">
-                                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                                              strokeWidth="2"
-                                                              d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21">
-                                                        </path>
-                                                    </svg>
-                                                </>
-                                            }
-                                        </button>
+                                        <ErrorMessage name="firstName" component="span" className="text-red-500" />
                                     </div>
-                                    <ErrorMessage name="password" component="span" className="text-red-500"/>
-                                </div>
-                                <div className="w-1/2">
-                                    <label htmlFor="confirmPassword">Confirm Password</label>
-                                    <div className="relative">
+                                    <div className="w-1/2">
+                                        <label htmlFor="lastName">Last Name</label>
                                         <Field
-                                            type={confirmPasswordVisible ? 'text' : 'password'}
-                                            id="confirmPassword"
-                                            name="confirmPassword"
+                                            type="text"
+                                            id="lastName"
+                                            name="lastName"
                                             className="w-full border border-gray-300 rounded px-3 py-2"
                                         />
-                                        <span
-                                            className="absolute top-2 right-2 cursor-pointer"
-                                            onClick={toggleConfirmPasswordVisibility}
-                                        >
+                                        <ErrorMessage name="lastName" component="span" className="text-red-500" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">Phone</label>
+                                    <Field type="text" id="phone" name="phone" className="w-full border border-gray-300 rounded px-3 py-2" />
+                                    <ErrorMessage name="phone" component="span" className="text-red-500" />
+                                </div>
+                                <div>
+                                    <label htmlFor="email">Email</label>
+                                    <Field
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        className="w-full border border-gray-300 rounded px-3 py-2"
+                                    />
+                                    <ErrorMessage name="email" component="span" className="text-red-500" />
+                                </div>
+                                <div>
+                                    <label htmlFor="username">Username</label>
+                                    <Field
+                                        type="text"
+                                        id="username"
+                                        name="username"
+                                        className="w-full border border-gray-300 rounded px-3 py-2"
+                                    />
+                                    <ErrorMessage name="username" component="span" className="text-red-500" />
+                                </div>
+
+                                <div className="flex space-x-4">
+                                    <div className="w-1/2">
+                                        <label htmlFor="password">Password</label>
+                                        <div className="relative">
+                                            <Field
+                                                type={passwordVisible ? 'text' : 'password'}
+                                                id="password"
+                                                name="password"
+                                                className="w-full border border-gray-300 rounded px-3 py-2"
+                                            />
+                                            <button
+                                                type="button"
+                                                className="absolute top-1/2 right-2 transform -translate-y-1/2"
+                                                onClick={togglePasswordVisibility}
+                                            >
+                                                {passwordVisible ?
+                                                    <>
+                                                        <svg className="w-6 h-6" fill="none" stroke="currentColor"
+                                                             viewBox="0 0 24 24"
+                                                             xmlns="http://www.w3.org/2000/svg">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
+                                                            </path>
+                                                        </svg>
+                                                    </>
+                                                    :
+                                                    <>
+                                                        <svg className="w-6 h-6" fill="none" stroke="currentColor"
+                                                             viewBox="0 0 24 24"
+                                                             xmlns="http://www.w3.org/2000/svg">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21">
+                                                            </path>
+                                                        </svg>
+                                                    </>
+                                                }
+                                            </button>
+                                        </div>
+                                        <ErrorMessage name="password" component="span" className="text-red-500"/>
+                                    </div>
+                                    <div className="w-1/2">
+                                        <label htmlFor="confirmPassword">Confirm Password</label>
+                                        <div className="relative">
+                                            <Field
+                                                type={confirmPasswordVisible ? 'text' : 'password'}
+                                                id="confirmPassword"
+                                                name="confirmPassword"
+                                                className="w-full border border-gray-300 rounded px-3 py-2"
+                                            />
+                                            <span
+                                                className="absolute top-2 right-2 cursor-pointer"
+                                                onClick={toggleConfirmPasswordVisibility}
+                                            >
                                             {confirmPasswordVisible ?
                                                 <>
                                                     <svg className="w-6 h-6" fill="none" stroke="currentColor"
@@ -207,30 +200,30 @@ const Register = () => {
                                                 </>
                                             }
                                         </span>
+                                        </div>
+                                        <ErrorMessage
+                                            name="confirmPassword"
+                                            component="span"
+                                            className="text-red-500"
+                                        />
                                     </div>
-                                    <ErrorMessage
-                                        name="confirmPassword"
-                                        component="span"
-                                        className="text-red-500"
-                                    />
                                 </div>
-                            </div>
-                            {error && <div className="text-red-700 bg-red-500"><p>{error}</p></div>}
-                            <button
-                                type="submit"
-                                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded "
-                                disabled={loading}
-                            >
-                                Create Account
-                            </button>
-                            {success && (
-                                <p className="text-green-500">Registration successful! Please proceed to the login page.</p>
-                            )}
-                        </Form>
-                    </Formik>
+                                {error && <div className="text-red-700 bg-red-500"><p>{error}</p></div>}
+                                <button
+                                    type="submit"
+                                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded "
+                                    disabled={loading}
+                                >
+                                    Create Account
+                                </button>
+                                {success && (
+                                    <p className="text-green-500">Registration successful! Please proceed to the login page.</p>
+                                )}
+                            </Form>
+                        </Formik>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 };
