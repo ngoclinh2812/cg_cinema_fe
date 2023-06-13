02ffb4b51f2c1d6471c55be90c7596db241efa8a
voquangvinhh <voquang059@gmail.com>
2023-06-13 10:09:33 +0700

    Vinh commit

--- src/components/theatre/TheatreListTab.js
+++ src/components/theatre/TheatreListTab.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Tabs, Tab, TabPanel, TabsBody, TabsHeader} from "@material-tailwind/react";
+import {Tab, TabPanel, Tabs, TabsBody, TabsHeader} from "@material-tailwind/react";
 import {
     getTheater,
     getTheaters,
@@ -10,16 +10,17 @@ import {
 } from "../../features/theater/theaterSlice";
 import {useDispatch, useSelector} from "react-redux";
 import "../../asset/styles/theater.css"
+import {Link} from "react-router-dom";
 
 
 const TheatreListTab = () => {
     const [loading, setLoading] = useState(false);
-    const [activeTab, setActiveTab] = React.useState("html");
+    const [activeTab, setActiveTab] = React.useState("Theater 1");
     const [theaters, setTheaters] = useState([]);
     const dispatch = useDispatch();
     const theaterList = useSelector(selectTheaterList);
     const success = useSelector(selectSuccess);
-    const [theaterId, setTheaterId] = useState();
+    const [theaterId, setTheaterId] = useState(1);
     const [theaterDt, setTheaterDt] = useState([]);
     const theaterDetail = useSelector(selectTheater);
 
@@ -36,27 +37,38 @@ const TheatreListTab = () => {
 
     useEffect(() => {
         getTheaterList();
-    }, [success, theaterList]);
+        getTheaterDetail(theaterId);
+
+    }, [success, theaterList, theaterId, theaterDetail]);
 
 
     useEffect(() =>{
         setLoading(true)
     }, [theaters])
 
-    useEffect(() => {
-        getTheaterDetail(theaterId);
-    }, [theaterId])
+    // useEffect(() => {
+    //     if(theaterId) {
+    //         getTheaterDetail(theaterId);
+    //     }
+    // }, [theaterId])
 
     const getTheaterDetail = async (theaterId) => {
-        if (success) {
-           await dispatch(getTheater(theaterId))
-            setTheaterDt(theaterDetail)
+        console.log('success: ',success)
+        if (!success) {
+            await dispatch(getTheater(theaterId))
+            console.log(theaterId)
+            if(theaterDetail) {
+                setTheaterDt(theaterDetail)
+            }
         } else {
             setTheaterDt(theaterDetail)
             dispatch(setSuccess(true))
         }
     }
 
+
+    console.log('theaterId: ', theaterId)
+    console.log('theaterDetail: ', theaterDetail)
 
     return (
         <div className="">
@@ -67,26 +79,22 @@ const TheatreListTab = () => {
                         className: "bg-transparent border-b-2 border-blue-500 shadow-none rounded-none",
                     }}
                 >
-                    {theaters !== null ? <>
-                        {theaters.map((theater) => (
-                            <Tab
-                                key={theater.id}
-                                value={theater.name}
-                                onClick={() => (setActiveTab(theater.name), setTheaterId(theater.id))
-                                }
-                                className={activeTab === theater.name ? "text-blue-500" : ""}
-                            >
-                                {theater.name}
-                            </Tab>
-                        ))}
-                    </> : <p>Something went wrong</p>}
-
+                    {theaters && theaters.map((theater, index) => (
+                        <Tab
+                            key={index}
+                            value={theater.name}
+                            onClick={() => (setActiveTab(theater.name), setTheaterId(theater.id))
+                            }
+                            className={activeTab === theater.name ? "text-blue-500" : ""}
+                        >
+                            {theater.name}
+                        </Tab>
+                    ))}
                 </TabsHeader>
                 <TabsBody>
-                    {theaters !== null ? <>
-                        {theaters.map((theater) => (
-                            <TabPanel key={theater.id} value={theater.name}>
-                                {theaterDt !== null ? <div className={"theater-body"}>
+                        {theaters && theaterDt &&  theaters.map((theater, index) => (
+                            <TabPanel key={index} value={theater.name}>
+                                <div className={"theater-body"}>
                                     <h1>
                                         {theater.name}
                                     </h1>
@@ -94,24 +102,20 @@ const TheatreListTab = () => {
                                         {theater.address}
                                     </p>
                                     <span className={"month"}>
-                                    <em>number </em>
-                                    <span>month and year {theaterId}</span>
-                                </span>
-
-                                    {theaterDt.map((theater) => (
-                                        <div>
+                                        <em>number </em>
+                                        <span>month and year {theaterId}</span>
+                                    </span>
+                                    {theaterDt.map((theater, index) => (
+                                        <div key={index}>
                                             {theater.movie_name}
                                             {theater.show_time}
                                         </div>
                                     ))}
-                                </div> : <p>Ban chua chon rap</p>}
-
+                                </div>
                             </TabPanel>
                         ))}
-                    </> : <p>Something went wrong</p>}
                 </TabsBody>
             </Tabs>
-
         </div>
     );
 };
