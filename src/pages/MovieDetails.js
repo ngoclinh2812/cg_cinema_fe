7f91dc86da6407cc1918608e36f06f791a06685c
ngoclinh2812 <hngoclinh2812@gmail.com>
2023-06-17 00:09:04 +0700

    linh update MovieDetails.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -11,10 +11,12 @@ const MovieDetails = () => {
   const loading = useSelector(selectLoading);
   const movieDetails = useSelector(selectMovieDetails);
   const error = useSelector(selectError);
+  const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+
 
     useEffect(() => {
       dispatch(fetchMovieDetails(movieId));
-  }, [movieId]);
+    }, [movieId]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -28,26 +30,28 @@ const MovieDetails = () => {
     console.log(movieDetails);
   return (
       <div className="flex justify-center items-center h-screen">
-          <div className="max-w-3xl bg-white p-8 rounded-lg shadow-md">
-              <div className="flex flex-col md:flex-row">
-                  <div className="w-full md:w-2/5">
-                      <div className="aspect-w-16 aspect-h-9">
-                          <iframe
-                              title="Movie Trailer"
-                              className="w-full h-full"
-                              src={movieDetails?.trailer}
-                              allowFullScreen
-                          />
+          <div className="flex flex-col items-center">
+              <div className="aspect-w-16 aspect-h-9 w-full">
+                  <iframe
+                      title="Movie Trailer"
+                      className="w-full h-full"
+                      src={`https://www.youtube.com/embed/${movieDetails?.trailer}`}
+                      allowFullScreen
+                  />
+              </div>
+              <div className="max-w-3xl bg-white p-8 rounded-lg shadow-md mt-6">
+                  <div className="flex flex-col md:flex-row">
+                      <div className="w-full md:w-3/5 md:pl-8">
+                          <img src={movieDetails?.img} alt="Movie Poster" /><br />
+                          <h4 className="text-3xl font-bold mb-4">{movieDetails?.name}</h4>
+                          <p className="text-gray-600 text-lg mb-4"><strong>Mô tả: </strong>{movieDetails?.description}</p>
+                          <p className="text-gray-600 text-lg mb-4"><strong>Thể loại: </strong>{movieDetails?.genre}</p>
+                          <p className="text-gray-600 text-lg mb-4"><strong>Ngày khởi chiếu: </strong>  {movieDetails?.dateStart && movieDetails?.dateStart.slice(0, 10)}
+                          </p>
+                          <Button color="teal" ripple="light">
+                              Buy Ticket
+                          </Button>
                       </div>
-                  </div>
-                  <div className="w-full md:w-3/5 mt-6 md:mt-0 md:pl-8">
-                      <h4 className="text-3xl font-bold mb-4">{movieDetails?.name}</h4>
-                      <p className="text-gray-600 text-lg mb-4">ID: {movieDetails?.id}</p>
-                      <p className="text-gray-600 text-lg mb-4">Mô tả: {movieDetails?.description}</p>
-                      <p className="text-gray-600 text-lg mb-4">Ngày khởi chiếu: {movieDetails?.dateStart}</p>
-                      <Button color="teal" ripple="light">
-                          Buy Ticket
-                      </Button>
                   </div>
               </div>
           </div>
